a53ba18114d0e18b1613fc57b50e20cfce6b7a92
Gerasimchuk Elena <zziikk2003@gmail.com>
2023-04-11 12:42:53 +0700

    Add keyboard hiding

--- Cinema/Views/AuthScreens/SignInScreen/SignInViewController.swift
+++ Cinema/Views/AuthScreens/SignInScreen/SignInViewController.swift
@@ -52,3 +52,19 @@ private extension SignInViewController {
         }
     }
 }
+
+private extension SignInViewController {
+    func setupToHideKeyboardOnTapOnView() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(
+            target: self,
+            action: #selector(dismissKeyboard(sender:)))
+        
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc
+    func dismissKeyboard(sender: AnyObject) {
+        view.endEditing(true)
+    }
+}
--- Cinema/Views/AuthScreens/SignUpScreen/SignUpViewController.swift
+++ Cinema/Views/AuthScreens/SignUpScreen/SignUpViewController.swift
@@ -50,3 +50,19 @@ final class SingUpViewController: UIViewController {
         }
     }
 }
+
+private extension SingUpViewController {
+    func setupToHideKeyboardOnTapOnView() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(
+            target: self,
+            action: #selector(dismissKeyboard(sender:)))
+        
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc
+    func dismissKeyboard(sender: AnyObject) {
+        view.endEditing(true)
+    }
+}
