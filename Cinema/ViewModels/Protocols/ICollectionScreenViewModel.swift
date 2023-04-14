9f3093c3c38927135498157462aa320370453a4d
Gerasimchuk Elena <zziikk2003@gmail.com>
2023-04-14 23:49:38 +0700

    Add view model for collection screens

---
+++ Cinema/ViewModels/Protocols/ICollectionScreenViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  ICollectionScreenViewModel.swift
+//  Cinema
+//
+//  Created by Елена on 14.04.2023.
+//
+
+import Foundation
+
+protocol ICollectionScreenViewModel {
+    func addNewCollection(collectionName: String, imageCollectionName: String)
+    
+    func updateCollection()
+    
+    func deleteCollection()
+}
